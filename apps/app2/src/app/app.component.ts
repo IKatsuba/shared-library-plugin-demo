e1af9b677407397f4ea27d4e54f583585221a812
Igor Katsuba <i.katsuba@tinkoff.ru>
2020-08-19 10:26:21 +0300

    refactor

--- apps/app2/src/app/app.component.ts
+++ apps/app2/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app2-root',
@@ -7,4 +8,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app2';
+
+  constructor(private http: HttpClient){
+    
+  }
 }
--- apps/app2/src/main.ts
+++ apps/app2/src/main.ts
@@ -5,7 +5,9 @@ import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
 if (environment.production) {
-  enableProdMode();
+  try {
+    enableProdMode();
+  } catch (e) {}
 }
 
 platformBrowserDynamic()
